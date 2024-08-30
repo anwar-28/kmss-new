ffd0a75fd856e725409200b838492f590751688b
anwar-28 <kiggundushaafie28@outlook.com>
2024-08-30 15:02:13 +0300

    me1

--- appwrite/config.ts
+++ appwrite/config.ts
@@ -1,7 +1,9 @@
 import { Client, Databases, Storage } from "appwrite";
 
+// Initialize the Appwrite client
 export const client = new Client();
 
+// Configuration object for Appwrite
 export const appwriteConfig = {
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID as string,
   endpointUrl: process.env.NEXT_PUBLIC_ENDPOINT_ID as string,
@@ -10,8 +12,11 @@ export const appwriteConfig = {
   articlesCollection: process.env.NEXT_PUBLIC_ARTICLES_COLLECTION_ID as string,
 };
 
-client.setEndpoint("https://cloud.appwrite.io/v1");
-client.setProject(appwriteConfig.projectId);
+// Set the Appwrite client configuration
+client
+  .setEndpoint(appwriteConfig.endpointUrl) // Set endpoint from config
+  .setProject(appwriteConfig.projectId); // Set project ID from config
 
+// Initialize and export Appwrite services
 export const storage = new Storage(client);
 export const databases = new Databases(client);
